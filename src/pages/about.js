c28db7556210480ef3a0bc1407648cb74eb730b2
Jason Lengstorf <jason@lengstorf.com>
2019-04-14 18:58:17 +0300

    feat: add pages and links

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+const About = () => (
+  <>
+    <h1>About Me</h1>
+    <p>This is my personal website.</p>
+    <Link to="/">&larr; back to home</Link>
+  </>
+);
+
+export default About;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
-export default () => <div>Hello Frontend Masters!</div>;
+export default () => (
+  <>
+    <h1>Home</h1>
+    <p>Hello Minnesota!</p>
+    <Link to="/about/">Learn about me &rarr;</Link>
+  </>
+);
