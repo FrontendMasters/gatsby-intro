e0fbdbf89c382ec7f84cf069fa6482959d326e14
Jason Lengstorf <jason@lengstorf.com>
2019-04-23 17:34:19 -0700

    feat: install and configure Emotion

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: 'Frontend Masters Gatsby Workshop',
     description:
-      'A site we built together during a full-day Frontend Masters Gatsby workshop!',
+      'A site we built together during a full-day Frontend Masters Gatsby workshop!'
   },
-  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
+  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet']
 };
