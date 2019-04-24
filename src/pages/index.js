79ca30085df5c7b0f32f110d2e7c1316df4cdfb2
Jason Lengstorf <jason@lengstorf.com>
2019-04-23 17:35:07 -0700

    feat: use the global layout for all pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
 export default () => (
-  <>
+  <Layout>
     <h1>Home</h1>
     <p>Hello Minnesota!</p>
     <Link to="/about/">Learn about me &rarr;</Link>
-  </>
+  </Layout>
 );
