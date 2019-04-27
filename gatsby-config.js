5345afe9df25c09fbd14a4da8000008526f3340e
Jason Lengstorf <jason@lengstorf.com>
2019-04-27 10:39:53 -0700

    feat: add the filesystem source plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,5 +15,12 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: 'posts',
+      },
+    },
   ],
 };
