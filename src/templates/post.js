2b71658edba95011349c618cbfe7db574ef8f385
Jason Lengstorf <jason@lengstorf.com>
2019-04-27 10:42:30 -0700

    feat: add a post template

---
+++ src/templates/post.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import { MDXRenderer } from 'gatsby-mdx';
+import { css } from '@emotion/core';
+import Layout from '../components/layout';
+import ReadLink from '../components/read-link';
+
+export const query = graphql`
+  query($slug: String!) {
+    mdx(fileAbsolutePath: { regex: $slug }) {
+      frontmatter {
+        title
+        author
+      }
+      code {
+        body
+      }
+    }
+  }
+`;
+
+const PostTemplate = ({ data: { mdx: post } }) => (
+  <Layout>
+    <h1>{post.frontmatter.title}</h1>
+    <p
+      css={css`
+        font-size: 0.75rem;
+      `}
+    >
+      Posted by {post.frontmatter.author}
+    </p>
+    <MDXRenderer>{post.code.body}</MDXRenderer>
+    <ReadLink to="/">&larr; back to all posts</ReadLink>
+  </Layout>
+);
+
+export default PostTemplate;
