bc1e57ade358d07ecf5e8e297720892d001ff5f6
Jason Lengstorf <jason@lengstorf.com>
2019-04-29 21:16:30 -0500

    chore: add a bundle analyzer for perf monitoring

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,5 +38,14 @@ module.exports = {
         username: 'gatsbyjs',
       },
     },
+    {
+      resolve: 'gatsby-plugin-webpack-bundle-analyzer',
+      options: {
+        production: true,
+        disable: !process.env.ANALYZE_BUNDLE_SIZE,
+        generateStatsFile: true,
+        analyzerMode: 'static',
+      },
+    },
   ],
 };
