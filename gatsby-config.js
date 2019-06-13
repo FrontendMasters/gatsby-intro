e899b56e16d4291ee2b67c3408c63a455a04ada2
Krista <k.marie.koch@gmail.com>
2019-06-13 09:56:18 -0500

    Removes conflict markers in gatsby-config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,11 +2,7 @@ module.exports = {
   siteMetadata: {
     title: 'Frontend Masters Gatsby Workshop',
     description:
-<<<<<<< HEAD
       'A site we built together during a full-day Frontend Masters Gatsby workshop!'
-=======
-      'A site we built together during a full-day Frontend Masters Gatsby workshop!',
->>>>>>> b6ceb4c9edba078dcacc3f3f902f37d0d242be3c
   },
   plugins: [
     'gatsby-plugin-emotion',
@@ -15,17 +11,9 @@ module.exports = {
       resolve: 'gatsby-mdx',
       options: {
         defaultLayouts: {
-<<<<<<< HEAD
           default: require.resolve('./src/components/layout.js')
         }
       }
     }
   ]
-=======
-          default: require.resolve('./src/components/layout.js'),
-        },
-      },
-    },
-  ],
->>>>>>> b6ceb4c9edba078dcacc3f3f902f37d0d242be3c
 };
