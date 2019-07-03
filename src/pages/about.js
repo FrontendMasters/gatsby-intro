7681b6e63ce9b8b349f930ce1658d3db32c9a3c1
Mark Hayes <mark@elegantseagulls.com>
2019-07-03 07:58:13 -0400

    adding in styles and the components directory.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../components/layouts';
 
 export default () => (
-	<>
+	<Layout>
 	<h1> About Me</h1>
 	<p>This is my personal website.</p>
-	</>
+	<Link to="/">&larr; back to home</Link>
+	</Layout>
 );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
 import React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../components/layouts';
 
-export default () => <div>Hi Frontend Masters! Dis Mark</div>;
+export default () => (
+	<Layout>
+		<h1>Hi Frontend Masters! Dis Mark</h1>
+		<p>Hello Michigan</p>
+		<Link to="/about/">Learn More About Me &rarr;</Link>
+	</Layout>
+);
