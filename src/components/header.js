41af9fb710fc672860727f6caa57f15e372fb79b
Mark Hayes <mark.m.hayes1@gmail.com>
2019-07-09 20:50:25 -0400

    adding the bablerc file

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import {  css } from '@emotion/core';
+import { css } from '@emotion/core';
 import { Link } from 'gatsby';
 
 const NavLink = styled(Link)`
@@ -22,22 +22,31 @@ const NavLink = styled(Link)`
 `;
 
 const Header = () => (
-	<header
-		css={css`
-			background: #eee;
-			border0bottom: 1px solid #ddd;
-			display: flex;
-			justify-content: space-between;
-			padding: 0.5rem calc((100vw - 550px - 0.5rem) /2);
-		`}
-	>
-		<NavLink to="/" fontWeight="bold">FEM Workshop</NavLink>
-		<nav css={css`margin-top: 0;`}>
-			<NavLink to="/" activeClassName="current-page">Home</NavLink>
-			<NavLink to="/about/" activeClassName="current-page">About</NavLink>
-		</nav>
-	</header>
-
+  <header
+    css={css`
+      background: #eee;
+      border0bottom: 1px solid #ddd;
+      display: flex;
+      justify-content: space-between;
+      padding: 0.5rem calc((100vw - 550px - 0.5rem) / 2);
+    `}
+  >
+    <NavLink to="/" fontWeight="bold">
+      FEM Workshop
+    </NavLink>
+    <nav
+      css={css`
+        margin-top: 0;
+      `}
+    >
+      <NavLink to="/" activeClassName="current-page">
+        Home
+      </NavLink>
+      <NavLink to="/about/" activeClassName="current-page">
+        About
+      </NavLink>
+    </nav>
+  </header>
 );
 
 export default Header;
