d648d0afcada547be0b4d0ed33a08814e873ca05
Marc Grabanski <marc@mjg.in>
2019-08-04 18:24:21 -0500

    Update dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     'gatsby-plugin-emotion',
     'gatsby-plugin-react-helmet',
     {
-      resolve: 'gatsby-mdx',
+      resolve: 'gatsby-plugin-mdx',
       options: {
         defaultLayouts: {
           default: require.resolve('./src/components/layout.js'),
