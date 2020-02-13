78819caa5b00725ed9fe7011bbc7e42ca810c2b5
HarryFray <harry.fray7@gmail.com>
2020-02-13 14:23:07 -0600

    feat(site): add images in markdown/mdx

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from '@emotion/styled';
 import { Link, graphql, useStaticQuery } from 'gatsby';
+import BackgroundImage from 'gatsby-background-image';
 
-const HeroImage = styled.div`
-  background-image: url('./images/mo.jpg');
-  height: 100px;
+const HeroImage = styled(BackgroundImage)`
+  height: 400px;
   background-position: top 20% center;
   background-size: cover;
   * {
@@ -13,8 +13,19 @@ const HeroImage = styled.div`
 `;
 
 const Hero = () => {
+  const { image } = useStaticQuery(graphql`
+    query {
+      image: file(relativePath: { eq: "mo.jpg" }) {
+        sharp: childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `);
   return (
-    <HeroImage>
+    <HeroImage fluid={image.sharp.fluid} fadeIn="soft">
       <h1>Front end matsters leanring gatsby</h1>
       <p>
         Hello Missouri! <Link to="about">learn about me</Link>
--- src/templates/post.js
+++ src/templates/post.js
@@ -3,6 +3,8 @@ import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 import ReadLink from '../components/readLink';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
+import Image from 'gatsby-image';
+import { css } from '@emotion/core';
 
 export const query = graphql`
   query($slug: String!) {
@@ -10,6 +12,13 @@ export const query = graphql`
       frontmatter {
         title
         author
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
       }
       body
     }
@@ -18,6 +27,13 @@ export const query = graphql`
 const PostTemplate = ({ data: { mdx: post } }) => {
   return (
     <Layout>
+      <Image
+        css={css`
+          height: 200px;
+        `}
+        alt={post.title}
+        fluid={post.frontmatter.image.childImageSharp.fluid}
+      />
       <h1>{post.frontmatter.title}</h1>
       <p>{`posted by', ${post.frontmatter.author}`} </p>
       <MDXRenderer>{post.body}</MDXRenderer>
