50b2016289f7878c1ecb745efe675f6c10fe3141
Travis Ricks <travis@travisricks.com>
2020-06-09 11:34:00 +0900

    create simple pages and routing

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+export default () => (
+  <>
+    <h1>About me</h1>
+    <p>this is my website</p>
+    <Link to="/">&larr; back to home</Link>
+  </>
+);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
-export default () => <div>Hello Frontend Masters!</div>;
+export default () => (
+  <>
+    <div>Hello there, Frontend Masters!</div>
+    <Link to="/about/">Learn about me &rarr;</Link>
+  </>
+);
