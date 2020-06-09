2be7dde6a046ea9f9e678184255420599e67a7b6
Travis Ricks <travis@travisricks.com>
2020-06-09 15:01:35 +0900

    add in a hero image component

---
+++ src/components/hero.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { Link, graphql, useStaticQuery } from 'gatsby';
+
+const ImageBackground = styled('div')`
+  background-image: url('/images/louie-martinez-tokyo.jpg');
+  background-position: top 45% center;
+  background-size: cover;
+  height: 40vh;
+
+  + * {
+    margin-top: 0;
+  }
+`;
+
+const TextBox = styled('div')`
+  background-image: linear-gradient(to top, #ddbbffdd 2rem, #ddbbff00);
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: flex-end;
+  padding: 0 calc((100vw - 550px) / 2) 2rem;
+  width: 100%;
+
+  h1 {
+    text-shadow: 1px 1px 3px #eeddff66;
+    font-size: 2.25rem;
+  }
+
+  p,
+  a {
+    color: #222;
+    margin-top: 0;
+  }
+
+  a {
+    margin-top: 0.5rem;
+  }
+`;
+
+const Hero = () => {
+  return (
+    <ImageBackground>
+      <TextBox>
+        <h1>FEM + Gatsby &hearts;</h1>
+        <p>
+          Hello Tokyo. <Link to="/about">Learn about me.</Link>
+        </p>
+      </TextBox>
+    </ImageBackground>
+  );
+};
+
+export default Hero;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,19 +3,23 @@ import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import usePosts from '../hooks/use-posts';
 import PostPreview from '../components/post-preview';
+import Hero from '../components/hero';
 
 export default () => {
   const posts = usePosts();
   return (
-    <Layout>
-      <h1>Hello there, Frontend Masters!</h1>
-      <p>this is a class</p>
-      <Link to="/about/">Learn about me &rarr;</Link>
+    <>
+      <Hero />
+      <Layout>
+        <h1>Hello there, Frontend Masters!</h1>
+        <p>this is a class</p>
+        <Link to="/about/">Learn about me &rarr;</Link>
 
-      <h2>Read my Blog</h2>
-      {posts.map(post => (
-        <PostPreview key={post.slug} post={post} />
-      ))}
-    </Layout>
+        <h2>Read my Blog</h2>
+        {posts.map(post => (
+          <PostPreview key={post.slug} post={post} />
+        ))}
+      </Layout>
+    </>
   );
 };
