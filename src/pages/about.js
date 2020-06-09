ec352b89f224ef62d297f1d3c4899c613b4fcfa0
Travis Ricks <travis@travisricks.com>
2020-06-09 11:51:46 +0900

    create base styles using emotion

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
 export default () => (
-  <>
+  <Layout>
     <h1>About me</h1>
     <p>this is my website</p>
     <Link to="/">&larr; back to home</Link>
-  </>
+  </Layout>
 );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
 export default () => (
-  <>
-    <div>Hello there, Frontend Masters!</div>
+  <Layout>
+    <h1>Hello there, Frontend Masters!</h1>
+    <p>this is a class</p>
     <Link to="/about/">Learn about me &rarr;</Link>
-  </>
+  </Layout>
 );
